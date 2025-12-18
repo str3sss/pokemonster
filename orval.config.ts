7b330f0660eab4388aa56136183ee54606c7173d
Dmitry Shmelev <punkreatine@gmail.com>
2025-12-18 15:25:44 +0300

    add openapi

--- orval.config.ts
+++ orval.config.ts
@@ -1,23 +1,25 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   pokemon: {
-    input: './api/poke.yml',
+    input: './api/openapi.yml',
+
     output: {
       mode: 'tags-split',
       target: 'src/api/generated',
-      schemas: 'api/generated',
+      schemas: 'src/api/models',
       client: 'react-query',
       mock: false,
-      override: {
-        mutator: {
-          path: './src/api/mutator.ts',
-          name: 'customInstance',
-        },
-        query: {
-          useQuery: true,
-          useInfinite: false,
-          useInfiniteQueryParam: 'offset',
-        },
-      },
+      // override: {
+      //   mutator: {
+      //     path: './src/api/mutator.ts',
+      //     name: 'customInstance',
+      //   },
+      //   query: {
+      //     useQuery: true,
+      //     useInfinite: false,
+      //     useInfiniteQueryParam: 'offset',
+      //   },
+      // },
     },
   },
 };
